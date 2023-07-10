5d4ab39467997e976f18165d39b14935f5d14303
DanyloDmytruk <h01oc0st225@gmail.com>
2023-07-10 19:44:57 +0300

    Added about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,5 +1,43 @@
 @extends('layouts.main')
 
 @section('main_content')
+    <p>
+        <i class="fa-regular fa-star"></i> PostsStar: An Open-Source Blog for Free Expression
 
+    <p><i class="fa-solid fa-star"></i> PostsStar is an open-source blog platform that provides a space for individuals to freely share their thoughts,
+        ideas, and texts with the world. It is designed to empower users and promote open dialogue and free expression.</p>
+
+    <p><i class="fa-solid fa-key"></i> Key Features: </p>
+    <ul>
+        <li>Open-Source Platform: PostsStar is built on open-source technology, allowing developers to contribute,
+            customize, and enhance the platform. The source code is freely available, enabling anyone to modify and adapt it
+            to suit their specific needs. </li>
+
+        <li>User-Friendly Interface: PostsStar offers a user-friendly interface that makes it easy for bloggers of all
+            levels to create, edit, and publish their content. The intuitive design ensures a seamless experience for both
+            writers and readers.</li>
+
+        <li>Free Expression: At PostsStar, freedom of expression is highly valued. Users have the freedom to express their
+            thoughts, opinions, and ideas without restrictions. Whether it's personal experiences, insightful articles, or
+            creative writing, everyone is encouraged to share their unique perspectives.</li>
+
+        <li>Community Engagement: PostsStar fosters a vibrant and engaged community of bloggers and readers. Users can
+            interact with one another through comments, likes, and shares, promoting discussions and connecting like-minded
+            individuals from diverse backgrounds.</li>
+
+        <li>Content Categorization: To help users discover and explore content, PostsStar incorporates a robust
+            categorization system. It allows blog posts to be organized into various categories, making it easier for
+            readers to find content that aligns with their interests.</li>
+
+        <li>Responsive Design: PostsStar is built with a responsive design, ensuring that the blog looks great and functions
+            well across different devices, including desktops, tablets, and smartphones. This enhances accessibility and
+            enables users to access and engage with the platform from anywhere.</li>
+
+        <li>Customization Options: The platform provides customization options, allowing bloggers to personalize their
+            blog's appearance. Users can choose from a range of themes, layouts, and color schemes to create a unique and
+            visually appealing blog.</li>
+    </ul>
+    </p>
+
+    <h4 class="text-primary">PostsStar was developed by Danylo Dmytruk and Viacheslav Zhulchynskyy</h4>
 @endsection
